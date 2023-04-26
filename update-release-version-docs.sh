65ef9d600a39591d9831c0ab3407bc5fceeb9cf0
Patrick Stephens <pat@calyptia.com>
2023-04-26 15:54:29 +0100

    workflows: update for Windows
    Signed-off-by: Patrick Stephens <pat@calyptia.com>

--- update-release-version-docs.sh
+++ update-release-version-docs.sh
@@ -29,18 +29,24 @@ if [[ -z "$MAJOR_VERSION" ]]; then
 fi
 
 # Add Docker after first line in the table
-sed_wrapper -i -e "/| -.*$/a | $NEW_VERSION-debug | x86\_64, arm64v8, arm32v7 | Release [v$NEW_VERSION](https://fluentbit.io/announcements/v$NEW_VERSION/) |" "$SCRIPT_DIR"/installation/docker.md
-sed_wrapper -i -e "/| -.*$/a | $NEW_VERSION | x86\_64, arm64v8, arm32v7 | Debug images |" "$SCRIPT_DIR"/installation/docker.md
+if grep -q "$NEW_VERSION" "$SCRIPT_DIR"/installation/docker.md; then
+    echo "Found $NEW_VERSION already in the Docker docs so skipping update"
+else
+    sed_wrapper -i -e "/| -.*$/a | $NEW_VERSION-debug | x86\_64, arm64v8, arm32v7 | Release [v$NEW_VERSION](https://fluentbit.io/announcements/v$NEW_VERSION/) |" "$SCRIPT_DIR"/installation/docker.md
+    sed_wrapper -i -e "/| -.*$/a | $NEW_VERSION | x86\_64, arm64v8, arm32v7 | Debug images |" "$SCRIPT_DIR"/installation/docker.md
+fi
 
 WIN_32_EXE_HASH=${WIN_32_EXE_HASH:?}
 WIN_32_ZIP_HASH=${WIN_32_ZIP_HASH:?}
 WIN_64_EXE_HASH=${WIN_64_EXE_HASH:?}
 WIN_64_ZIP_HASH=${WIN_64_ZIP_HASH:?}
 
-sed_wrapper -i -e "/The latest stable version is [0-9]+\.[0-9]+\.[0-9]+/The latest stable version is $NEW_VERSION/" "$SCRIPT_DIR"/installation/windows.md
-sed_wrapper -i -e "/fluent-bit-[0-9]+\.[0-9]+\.[0-9]+-win/fluent-bit-$NEW_VERSION-win/" "$SCRIPT_DIR"/installation/windows.md
+sed_wrapper -i -e "s/The latest stable version is .*,/The latest stable version is $NEW_VERSION/g" "$SCRIPT_DIR"/installation/windows.md
+sed_wrapper -i -e "s/fluent-bit-[0-9\.]*-win/fluent-bit-$NEW_VERSION-win/g" "$SCRIPT_DIR"/installation/windows.md
 
-sed_wrapper -i -e "/win32.exe) | \[[.*]\(https\]/win32.exe) | \[$WIN_32_EXE_HASH\]\(https" "$SCRIPT_DIR"/installation/windows.md
-sed_wrapper -i -e "/win32.zip) | \[[.*]\(https\]/win32.zip) | \[$WIN_32_ZIP_HASH\]\(https" "$SCRIPT_DIR"/installation/windows.md
-sed_wrapper -i -e "/win64.exe) | \[[.*]\(https\]/win64.exe) | \[$WIN_64_EXE_HASH\]\(https" "$SCRIPT_DIR"/installation/windows.md
-sed_wrapper -i -e "/win64.zip) | \[[.*]\(https\]/win64.zip) | \[$WIN_64_ZIP_HASH\]\(https" "$SCRIPT_DIR"/installation/windows.md
+sed_wrapper -i -e "s|releases.fluentbit.io/[0-9\.]*/|releases.fluentbit.io/$MAJOR_VERSION/|g" "$SCRIPT_DIR"/installation/windows.md
+
+sed_wrapper -i -e "s/win32.exe) | \[.*\]/win32.exe) | \[$WIN_32_EXE_HASH\]/g" "$SCRIPT_DIR"/installation/windows.md
+sed_wrapper -i -e "s/win32.zip) | \[.*\]/win32.zip) | \[$WIN_32_ZIP_HASH\]/g" "$SCRIPT_DIR"/installation/windows.md
+sed_wrapper -i -e "s/win64.exe) | \[.*\]/win64.exe) | \[$WIN_64_EXE_HASH\]/g" "$SCRIPT_DIR"/installation/windows.md
+sed_wrapper -i -e "s/win64.zip) | \[.*\]/win64.zip) | \[$WIN_64_ZIP_HASH\]/g" "$SCRIPT_DIR"/installation/windows.md
